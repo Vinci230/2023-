5940bba55923930142d884f57cf20cea8d1b7835
Vinci230 <lienxithy@gmail.com>
2024-01-27 20:30:59 +0800

    clone 并print关于commit的信息

--- changelog.py
+++ changelog.py
@@ -4,13 +4,18 @@ import subprocess
 
 def get_commits_for_day(repo_url, day):
     # 通过 git.remote.Remote 访问远程仓库
-    repo = git.Repo.clone_from(repo_url, 'temp_repo', branch='main', depth=1)
+    repo = git.Repo.clone_from(repo_url, 'temp_repo', branch='main')
 
     # 获取昨天和今天的日期范围
     start_date = datetime.strptime(day, "%Y-%m-%d")
+    # print(start_date)
     end_date = start_date + timedelta(days=1)
+    print(end_date)
     # 获取指定日期范围内的提交
-    commits = repo.iter_commits(rev='main', since=start_date, until=end_date)
+    commits = repo.iter_commits(rev='main',since=start_date)
+    print(commits)
+    # for commit in commits:
+    #     print(f"Commit ID: {commit.hexsha}")
 
     # 遍历指定日期范围内的提交
     for commit in commits:
@@ -19,7 +24,7 @@ def get_commits_for_day(repo_url, day):
         print(f"Date: {commit.authored_datetime}")
         print(f"Message: {commit.message}")
         print("\n")
-
+    print('over!')
 
 def main():
     repo_url = 'https://github.com/Vinci230/2024-lanshanexam.git'  # 替换为你的 HTTPS 形式的仓库 URL
